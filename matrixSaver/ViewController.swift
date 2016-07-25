c3900690006fb5e1bdf5318ed5d8c7e057e2ffdf
4oby <cvviktor@gmail.com>
2016-07-25 12:57:39 +0300

    first workind prototype

--- matrixSaver/ViewController.swift
+++ matrixSaver/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
+  @IBOutlet weak var webView: UIWebView!
 
 }
 
+extension ViewController {
+
+  override func viewDidLoad() {
+      showTheDammPage()
+  }
+
+  
+  func showTheDammPage() -> Void {
+    let htmlFile = NSBundle.mainBundle().pathForResource("index", ofType: "html")
+    do {
+    let htmlString = try NSString(contentsOfFile: htmlFile!, encoding: NSUTF16StringEncoding)
+    webView.loadHTMLString(String(htmlString), baseURL: NSBundle.mainBundle().bundleURL)
+    } catch {
+    
+    }
+  }
+}
